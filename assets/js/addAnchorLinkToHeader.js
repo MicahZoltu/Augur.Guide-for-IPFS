3d3e2c3ec29b4219fa042e2c5050928b68a16c64
CBB <crystalballbe@gmail.com>
2020-11-02 17:07:35 +0900

    Remove anchor link from version lanbel

--- assets/js/addAnchorLinkToHeader.js
+++ assets/js/addAnchorLinkToHeader.js
@@ -4,4 +4,4 @@ In order to add deep anchor links to existing page content, using "AnchorJS".
 https://github.com/bryanbraun/anchorjs
 */
 anchors.options.placement = "left"
-anchors.add();
+anchors.add('h3:not(.no-anchor)');
