49670328806c94699fe3d3118fdebdd6e9f519fa
Micah Zoltu <rink_untimed826@simplelogin.com>
2025-06-08 07:21:39 +0000

    Removes original Jekyll content, and renames content directory to app.

--- _site/assets/js/addAnchorLinkToHeader.js
+++
@@ -1,6 +0,0 @@
-/*
-In order to add deep anchor links to existing page content, using "AnchorJS".
-https://github.com/bryanbraun/anchorjs
-*/
-anchors.options.placement = "left";
-anchors.add().add('h1').remove(".no-anchor");
--- _site/assets/js/augur-ui/htmlElementCreators/index.js
+++
@@ -1,3 +0,0 @@
-export * as TableAugurUiOnEns from "./tableAugurUiOnENS.js";
-export * as TableOfficialRelease from "./tableOfficialRelease.js";
-export * as TableQuickAccess from "./tableQuickAccess.js";
